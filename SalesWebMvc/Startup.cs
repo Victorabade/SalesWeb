3c46837e1472e8bbab4e3a61fcd3604da8f68cf1
Victor Eduardo <victorabadevp@gmial.com>
2022-08-15 11:22:23 -0300

    Seending Service

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -39,15 +39,16 @@ namespace SalesWebMvc
             var connectionString = Configuration["connectionStrings:SalesWebMvcContext"];
             services.AddDbContext<SalesWebMvcContext>(options =>
                     options.UseNpgsql(connectionString));
-            
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
